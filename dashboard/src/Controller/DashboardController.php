8e6f8c546ac75d525307e011694459cfc4878267
W137025 <hugo.dupuis@worldline.com>
2024-10-15 11:16:36 +0200

    Fix / path

--- dashboard/src/Controller/DashboardController.php
+++ dashboard/src/Controller/DashboardController.php
@@ -3,17 +3,28 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DashboardController extends AbstractController
 {
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
-        return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
-        ]);
+        if ($this->security->getUser()) {
+            return new RedirectResponse($this->generateUrl('app_perso'));
+        } else {
+            return new RedirectResponse($this->generateUrl('app_login'));
+        }
     }
 
     #[Route('/perso', name: 'app_perso')]
