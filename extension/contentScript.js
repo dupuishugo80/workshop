b8eb98fd271fcda67b709fe119fbeff5223d6502
Wigloh <mrwigloh@gmail.com>
2024-10-16 15:42:55 +0200

    text color red

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -3,6 +3,10 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         const links = getAllLinks();
         sendResponse({ links: links });
     }
+    // Ajouter un écouteur pour les liens malveillants
+    if (request.message === 'highlightMaliciousLinks' && request.data) {
+        highlightMaliciousLinks(request.data);
+    }
 });
 
 function getAllLinks() {
@@ -17,4 +21,29 @@ function getAllLinks() {
     });
 
     return linkArray;
+}
+
+function highlightMaliciousLinks(result) {
+    console.log(result);
+    if (result && result.matches && result.matches.length > 0) {
+        result.matches.forEach(match => {
+            const url = match.threat.url;
+            const threatType = match.threatType;
+
+            if (threatType === 'MALWARE' || threatType === 'SOCIAL_ENGINEERING' || threatType === 'UNWANTED_SOFTWARE' || threatType === 'POTENTIALLY_HARMFUL_APPLICATION') {
+                const links = document.querySelectorAll('a');
+
+                links.forEach(link => {
+                    if (link.href === url) {
+                        link.style.color = 'red'; // Change la couleur du lien en rouge
+                        link.style.fontWeight = 'bold'; // Optionnel: met le texte en gras
+                        link.setAttribute('title', 'Attention : Ce lien est potentiellement dangereux.');
+
+                        // Optionnel : Ajout d'un avertissement visuel avec du CSS
+                        link.classList.add('malicious-link');
+                    }
+                });
+            }
+        });
+    }
 }
