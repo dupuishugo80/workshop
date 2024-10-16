b7ca19b88fd260ebdfcc9e6c9687c1c9c9b7fbac
W137025 <hugo.dupuis@worldline.com>
2024-10-16 14:14:34 +0200

    Signalement fonctionnel + Checkurl

--- dashboard/src/Controller/SignalementController.php
+++ dashboard/src/Controller/SignalementController.php
@@ -2,17 +2,61 @@
 
 namespace App\Controller;
 
+use App\Entity\Threat;
+use App\Entity\TypeThreat;
+use App\Form\ThreatFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SignalementController extends AbstractController
 {
     #[Route('/signalement', name: 'app_signalement')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $threat = new Threat();
+        $form = $this->createForm(ThreatFormType::class, $threat);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($threat);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_perso');
+        }
+
         return $this->render('signalement/index.html.twig', [
-            'controller_name' => 'SignalementController',
+            'threatForm' => $form,
         ]);
+    }
+
+    #[Route('/signalement/checkurl', name: 'check_url')]
+    public function checkUrl(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $url = $request->query->get('url');
+
+        if (!$url) {
+            return new JsonResponse(['error' => 'No URL provided'], 400);
+        }
+
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            return new JsonResponse(['error' => 'Invalid URL'], 400);
+        }
+
+        $typeThreats = $entityManager->getRepository(TypeThreat::class)->findAll();
+
+        $result = [];
+
+        foreach ($typeThreats as $typeThreat) {
+            $count = $entityManager->getRepository(Threat::class)->count([
+                'url' => $url,
+                'type' => $typeThreat
+            ]);
+            $result[strtolower($typeThreat->getName())] = $count;
+        }
+
+        return new JsonResponse($result);
     }
 }
---
+++ workshop.sql
@@ -0,0 +1,205 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 16 oct. 2024 à 14:13
+-- Version du serveur : 10.4.28-MariaDB
+-- Version de PHP : 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `workshop2024`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctrine_migration_versions`
+--
+
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20241015081748', '2024-10-15 10:17:58', 49),
+('DoctrineMigrations\\Version20241016081042', '2024-10-16 10:11:12', 103);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messenger_messages`
+--
+
+CREATE TABLE `messenger_messages` (
+  `id` bigint(20) NOT NULL,
+  `body` longtext NOT NULL,
+  `headers` longtext NOT NULL,
+  `queue_name` varchar(190) NOT NULL,
+  `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `available_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `delivered_at` datetime DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `threat`
+--
+
+CREATE TABLE `threat` (
+  `id` int(11) NOT NULL,
+  `type_id` int(11) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `added_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `threat`
+--
+
+INSERT INTO `threat` (`id`, `type_id`, `url`, `added_date`) VALUES
+(1, 1, 'http://test.com', '2024-11-01 10:41:00'),
+(2, 2, 'http://test.com', '2024-10-16 13:59:00'),
+(3, 2, 'http://test.com', '2024-10-16 14:11:56'),
+(4, 3, 'http://autre.com', '2024-10-16 14:12:13');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `type_threat`
+--
+
+CREATE TABLE `type_threat` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `warning_level` int(11) NOT NULL,
+  `warning_color` varchar(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `type_threat`
+--
+
+INSERT INTO `type_threat` (`id`, `name`, `warning_level`, `warning_color`) VALUES
+(1, 'Phishing', 3, '#ff0000'),
+(2, 'Malware', 5, '#ff0000'),
+(3, 'virus', 10, '#ff0000');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(180) NOT NULL,
+  `roles` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '(DC2Type:json)' CHECK (json_valid(`roles`)),
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `roles`, `password`) VALUES
+(1, 'test@test.fr', '[]', '$2y$13$YI6omkP0fpZIile5nUawM.aNPUUgkBDYTjYndOxEzOKDqKgc5tMf2');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `doctrine_migration_versions`
+--
+ALTER TABLE `doctrine_migration_versions`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Index pour la table `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
+  ADD KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
+  ADD KEY `IDX_75EA56E016BA31DB` (`delivered_at`);
+
+--
+-- Index pour la table `threat`
+--
+ALTER TABLE `threat`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_2C975CE7C54C8C93` (`type_id`);
+
+--
+-- Index pour la table `type_threat`
+--
+ALTER TABLE `type_threat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_IDENTIFIER_EMAIL` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `threat`
+--
+ALTER TABLE `threat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `type_threat`
+--
+ALTER TABLE `type_threat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `threat`
+--
+ALTER TABLE `threat`
+  ADD CONSTRAINT `FK_2C975CE7C54C8C93` FOREIGN KEY (`type_id`) REFERENCES `type_threat` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
