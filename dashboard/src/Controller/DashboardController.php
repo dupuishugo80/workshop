69a422a2026b1d028a7d723cfded83b138489741
DJENDER <medhi.djender@worldline.com>
2024-10-17 08:32:47 +0200

    add config for dashboardcontroller

--- dashboard/src/Controller/DashboardController.php
+++ dashboard/src/Controller/DashboardController.php
@@ -2,36 +2,51 @@
 
 namespace App\Controller;
 
+use App\Repository\LogsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
     private $security;
+    private $logRepository;
 
-    public function __construct(Security $security)
+    public function __construct(Security $security, LogsRepository $logRepository)
     {
         $this->security = $security;
+        $this->logRepository = $logRepository;
     }
 
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
+        // Redirection selon la connexion de l'utilisateur
         if ($this->security->getUser()) {
-            return new RedirectResponse($this->generateUrl('app_perso'));
+            return $this->redirectToRoute('app_perso');
         } else {
-            return new RedirectResponse($this->generateUrl('app_login'));
+            return $this->redirectToRoute('app_login');
         }
     }
 
     #[Route('/perso', name: 'app_perso')]
     public function perso(): Response
     {
+        // Récupérer l'utilisateur connecté
+        $user = $this->security->getUser();
+
+        // Si l'utilisateur est connecté, récupérer ses logs
+        if ($user) {
+            $logs = $this->logRepository->findBy(['user' => $user]);
+        } else {
+            $logs = [];
+        }
+
+        // Envoyer les logs à la vue
         return $this->render('dashboard/perso.html.twig', [
             'controller_name' => 'DashboardController',
+            'logs' => $logs,  // On envoie les logs à la vue Twig
         ]);
     }
 }
